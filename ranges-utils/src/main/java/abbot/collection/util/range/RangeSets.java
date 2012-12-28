603f4d9808dc4f936dbc6c90532d70856b8b0b89
Alan Wagner <ceraw79@hotmail.com>
2012-12-28 07:03:09 -0500

    Added utility methods for intersection and union of range sets

---
+++ ranges-utils/src/main/java/abbot/collection/util/range/RangeSets.java
@@ -0,0 +1,40 @@
+package abbot.collection.util.range;
+
+
+import com.google.common.collect.DiscreteDomain;
+
+public class RangeSets {
+
+
+    public static <T extends Comparable<T>> RangeSet<T> union(RangeSet<T> ... rangeSets) {
+        TreeRangeSet<T> result = new TreeRangeSet<T>();
+        for (RangeSet<T> rangeSet : rangeSets)
+            result.add(rangeSet);
+
+        return result;
+    }
+
+    public static <T extends Comparable<T>> RangeSet<T> union(DiscreteDomain<T> domain, RangeSet<T> ... rangeSets) {
+        DiscreteTreeRangeSet<T> result = new DiscreteTreeRangeSet<T>(domain);
+        for (RangeSet<T> rangeSet : rangeSets)
+            result.add(rangeSet);
+
+        return result;
+    }
+
+    public static <T extends Comparable<T>> RangeSet<T> intersection(RangeSet<T> ... rangeSets) {
+        TreeRangeSet<T> result = new TreeRangeSet<T>();
+        for (RangeSet<T> rangeSet : rangeSets)
+            result.add(rangeSet.complement());
+
+        return result.complement();
+    }
+
+    public static <T extends Comparable<T>> RangeSet<T> intersection(DiscreteDomain<T> domain, RangeSet<T> ... rangeSets) {
+        DiscreteTreeRangeSet<T> result = new DiscreteTreeRangeSet<T>(domain);
+        for (RangeSet<T> rangeSet : rangeSets)
+            result.add(rangeSet.complement());
+
+        return result.complement();
+    }
+}
